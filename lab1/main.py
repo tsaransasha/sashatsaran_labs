0d6c1f4b8621dfd27f4f174cd9b69faf0029eb35
STADFG <tsaransasha2008@gmail.com>
2026-03-24 09:16:04 +0200

    file changed 24.03

--- lab1/main.py
+++ lab1/main.py
@@ -8,15 +8,15 @@ class Triangle:
         self.b = float(b)
         self.c = float(c)
         self.name = "Triangle"
+        self.is_exists = (self.a > 0 and self.b > 0 and self.c > 0 and
+                          self.a + self.b > self.c and
+                          self.a + self.c > self.b and
+                          self.b + self.c > self.a)
 
     def perim(self):
-        if self.a + self.b <= self.c or self.a + self.c <= self.b or self.b + self.c <= self.a:
-            return 0
         return self.a + self.b + self.c
 
     def area(self):
-        if self.perim() == 0:
-            return 0
         p = self.perim() / 2
         return math.sqrt(p * (p - self.a) * (p - self.b) * (p - self.c))
 
@@ -26,6 +26,8 @@ class Rectangle:
         self.a = float(a)
         self.b = float(b)
         self.name = "Rectangle"
+        # Прямокутник існує, якщо сторони додатні
+        self.is_exists = self.a > 0 and self.b > 0
 
     def perim(self):
         return 2 * (self.a + self.b)
@@ -42,22 +44,22 @@ class Trapeze:
         self.d = float(d)
         self.name = "Trapeze"
 
+        diff = abs(self.a - self.b)
+        self.is_exists = (all(x > 0 for x in [self.a, self.b, self.c, self.d]) and
+                          diff < (self.c + self.d) and
+                          diff > abs(self.c - self.d) and
+                          diff != 0)
+
     def perim(self):
         return self.a + self.b + self.c + self.d
 
     def area(self):
         diff = abs(self.a - self.b)
-        if diff == 0:
-            return 0
-
-        x = (diff ** 2 + self.c ** 2 - self.d ** 2) / (2 * diff)
-        h2 = self.c ** 2 - x ** 2
-
-        if h2 < 0:
-            return 0
-
-        h = math.sqrt(h2)
-        return ((self.a + self.b) / 2) * h
+        s = (self.a + self.b) / 2
+        h_part = ((-diff + self.c + self.d) * (diff + self.c - self.d) * (diff - self.c + self.d) * (
+                diff + self.c + self.d))
+        h = (1 / (2 * diff)) * math.sqrt(h_part)
+        return s * h
 
 
 class Parallelogram:
@@ -66,6 +68,8 @@ class Parallelogram:
         self.b = float(b)
         self.h = float(h)
         self.name = "Parallelogram"
+
+        self.is_exists = self.a > 0 and self.b > 0 and 0 < self.h <= self.b
 
     def perim(self):
         return 2 * (self.a + self.b)
@@ -78,6 +82,7 @@ class Circle:
     def __init__(self, r):
         self.r = float(r)
         self.name = "Circle"
+        self.is_exists = self.r > 0
 
     def perim(self):
         return 2 * math.pi * self.r
@@ -95,17 +100,21 @@ def create_figure(line):
     params = parts[1:]
 
     try:
+        fig = None
         if figure_type == "Triangle" and len(params) == 3:
-            return Triangle(*params)
+            fig = Triangle(*params)
         elif figure_type == "Rectangle" and len(params) == 2:
-            return Rectangle(*params)
+            fig = Rectangle(*params)
         elif figure_type == "Trapeze" and len(params) == 4:
-            return Trapeze(*params)
+            fig = Trapeze(*params)
         elif figure_type == "Parallelogram" and len(params) == 3:
-            return Parallelogram(*params)
+            fig = Parallelogram(*params)
         elif figure_type == "Circle" and len(params) == 1:
-            return Circle(*params)
-    except ValueError:
+            fig = Circle(*params)
+
+        if fig and fig.is_exists:
+            return fig
+    except (ValueError, ZeroDivisionError):
         return None
 
     return None
